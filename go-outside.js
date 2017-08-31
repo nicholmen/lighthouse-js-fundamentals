b5086ace3894c15171895f265f3fba95da05ceff
Thomas Nicholson <nicholmen@gmail.com>
2017-08-31 19:30:34 +0000

    Adds looping exercises

--- go-outside.js
+++ go-outside.js
@@ -3,7 +3,7 @@ var temperature = -69;
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
+  console.log("Short sleeves w1on't cut it!");
 } else {
   console.log("Short sleeves are fine.");
 }
---
+++ test.js
@@ -0,0 +1,7 @@
+var seasons = ["spring", "summer", "fall"];
+var length = seasons.length;
+
+seasons.push("winter");
+
+console.log(length);
+console.log(seasons.length);
